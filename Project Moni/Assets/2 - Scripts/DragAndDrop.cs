6f44dde8af081debca2cb220133242f36c388c69
Michelle Santiago <michelle.santiago10@gmail.com>
2019-09-08 15:46:37 -0300

    Fisicas sem tretar saindo do carrinho

---
+++ Project Moni/Assets/2 - Scripts/DragAndDrop.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    // pode arrastar os itens restritamente dentro do carrinho
+
+    private bool dragging = false;
+    private float distance;
+
+    void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Update()
+    {
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+
+            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+            rb.MovePosition(rayPoint);
+        }
+    }
+}
