c968df5445465fcb7f9e6d744b9d1e74d21a3a72
Bernardo <behornerl@gmail.com>
2019-09-08 17:21:21 -0300

    Mudança nome scriptable objects Mercadoria -> MercadoriaStats

---
+++ Project Moni/Assets/2 - Scripts/MercadoriaStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Nova Mercadoria", menuName = "Mercadoria")]
+public class MercadoriaStats : ScriptableObject
+{
+
+    public GameObject MercadoriaPrefab;
+    public int Value;
+}
