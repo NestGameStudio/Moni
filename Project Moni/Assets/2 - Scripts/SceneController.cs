7902e2cddba2b292125c427d23dbfd884ba9840d
Bernardo <behornerl@gmail.com>
2019-09-10 02:26:53 -0300

    Renomeando SceneManager para SceneController (se não, sobrescreve a classe do Unity)

---
+++ Project Moni/Assets/2 - Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void ReloadCurrentScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
